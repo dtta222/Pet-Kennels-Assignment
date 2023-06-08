eaadac9a53dd9bcda556643f744aa6d4927212e3
dtta222 <100861465+dtta222@users.noreply.github.com>
2023-06-08 12:14:27 -0400

    Update KennelDriver.java

--- KennelDriver.java
+++ KennelDriver.java
@@ -1,14 +1,11 @@
 //*************************************************************************
  //KennelDriver.java
- //Date: 03/10/2023
  //Author: David Taylor
- //Class: CIT 149–19Z1–78178
- //Instructor: Krishna Nandanoor
  //Purpose: Develop a Java application where you will help accommodate and place three dogs in a large
 //Kennel in their own big crate. A program is needed which can take in the data of the dogs and assign
 //them to an available crate in the Kennel in a specific order and print the details of the Kennel and the dogs assigned to
 //the crate once all the crates have been filled.
-
+//
 // The large dog kennel has four crates used to transport pet animals across small distances in an animal shelter. Each crate
 //can accommodate one dog. The fourth crate cannot be used because it needs to be used for storage purposes. So only
 //THREE dogs can be accommodated per Kennel
